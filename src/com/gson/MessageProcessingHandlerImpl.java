42ef069b83a32d174e9340aae957d1a880c36b97
helloworldhelloworld <xiajie198912@gmail.com>
2015-03-18 23:59:37 +0800

    first

---
+++ src/com/gson/MessageProcessingHandlerImpl.java
@@ -0,0 +1,81 @@
+package com.gson;
+
+import com.gson.bean.InMessage;
+import com.gson.bean.OutMessage;
+import com.gson.inf.MessageProcessingHandler;
+
+public class MessageProcessingHandlerImpl implements MessageProcessingHandler{
+
+	@Override
+	public void allType(InMessage msg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void textTypeMsg(InMessage msg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void locationTypeMsg(InMessage msg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void imageTypeMsg(InMessage msg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void videoTypeMsg(InMessage msg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void linkTypeMsg(InMessage msg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void voiceTypeMsg(InMessage msg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void verifyTypeMsg(InMessage msg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void eventTypeMsg(InMessage msg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterProcess(InMessage inMsg, OutMessage outMsg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setOutMessage(OutMessage outMessage) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public OutMessage getOutMessage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
